e572316a76ef8eeea7a7f72908dfb1c95bf89493
IElf <qepwqtxs@qq.com>
2015-08-04 11:43:04 +0800

    Access-Control-Allow-Origin

---
+++ WebAPI/App_Start/JsonCallbackAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Web.Http.Filters;
+
+namespace WebAPI
+{
+    public class JsonCallbackAttribute:ActionFilterAttribute
+    {
+        private const string CallbackQueryParameter = "callback";
+
+        public override void OnActionExecuted(HttpActionExecutedContext context)
+        {
+           var jsonBuilder=new StringBuilder(null);
+                jsonBuilder.AppendFormat("({0})'", context.Response.Content.ReadAsStringAsync().Result);
+                context.Response.Content = new StringContent(jsonBuilder.ToString());
+                base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Http;
+using Elf;
+
+namespace WebAPI.Controllers
+{
+
+    public class ValuesController : ApiController
+    {
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+        [HttpGet]
+        public IEnumerable<DataBase> Get(string name, bool look)
+        {
+            return Iris.GetDataBases(look, name);
+        }
+        [HttpGet]
+        public IEnumerable<DataBase> Get(bool look)
+        {
+            return Iris.GetDataBases(look, "");
+        }
+        [HttpGet]
+        public IEnumerable<DataBase> Get()
+        {
+            return Iris.GetDataBases(false, "");
+        }
+    }
+}
