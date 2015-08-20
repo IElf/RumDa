2f04ef5d63d044db5bc82e58be57351bf674c0c5
IElf <qepwqtxs@qq.com>
2015-08-20 11:55:53 +0800

    commit

---
+++ Elf/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elf
+{
+    public class Module
+    {
+        public string Id { get { return Guid.NewGuid().ToString(); } }
+        public string ModuleCh { get; set; }
+        public string ModuleEn { get; set; }
+
+        public string InvolveTable { get; set; }
+
+        public string Description { get; set; }
+    }
+}
